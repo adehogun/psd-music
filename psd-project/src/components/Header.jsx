6ceb5c1b51c336a2c4c9dbf5f98c445e207aa2b8
patrick-231 <137486318+patrick-231@users.noreply.github.com>
2024-04-15 14:05:54 +0200

    Nav / Hero Section

---
+++ psd-project/src/components/Header.jsx
@@ -0,0 +1,29 @@
+
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between p-4">
+      <div className="logo">
+        <span className="font-bold text-xl ml-9">DEETIFY</span>
+      </div>
+      <ul className="flex space-x-8 font-semi-bold text-lg mr-10 gap-4">
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#">LIBRARY</a>
+        </li>
+        <li>
+          <a href="#">UPGRADE</a>
+        </li>
+        <li>
+          <button className='text-white bg-black rounded-md pb-2 px-8  hover:bg-white hover:text-black transition duration-300 ease-in-out text-center'>
+            Login
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ psd-project/src/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+
+
+const HeroSection = () => {
+  return (
+    <>
+     <div>
+        <h1 className='text-5xl text-center p-32 bg-gray-200 '>We're <em>the best choice for all your <br /> rhythms!</em></h1>
+        </div>
+        <div className='flex justify-center gap-5 bg-gray-200 pb-8'>
+            <button className='text-white bg-black rounded-lg px-12 font-semi-bold hover:bg-white hover:text-black transition duration-300 ease-in-out'>Live Shows</button>
+            <button className='text-white bg-black rounded-lg px-12 font-semi-bold hover:bg-white hover:text-black transition duration-300 ease-in-out'>Music from Across <br /> World</button>
+            <button className='text-white bg-black rounded-lg px-12 font-semi-bold hover:bg-white hover:text-black transition duration-300 ease-in-out'>See Music from <br /> Location</button>
+        </div>
+    </>
+  )
+} 
+
+export default HeroSection
