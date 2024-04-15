98bfc2734c1e34b622a58de3d0f96dc81d3fa024
patrick-231 <137486318+patrick-231@users.noreply.github.com>
2024-04-15 15:49:42 +0200

    styling in header and footer

--- psd-project/src/components/Footer.jsx
+++ psd-project/src/components/Footer.jsx
@@ -4,41 +4,47 @@ function Footer() {
   return (
     <footer className="grid gap-8 p-9"> 
       {/* First row */}
-      <div className="flex justify-between">
+      <div className="flex justify-between p-12">
         {/* About Us */}
         <div className="p-1 box bg-transparent p-5 ">
-          <h6 className="text-lg">About Us</h6>
+          <h6 className="text-lg font-semibold">About Us</h6>
           <ul>
-            <li><a href="/blog" className="text-blue-500 hover:text-blue-700 font-serif">Blog</a></li>
-            <li><a href="/about" className="text-blue-500 hover:text-blue-700 font-serif">About Us</a></li>
-            <li><a href="/contact" className="text-blue-500 hover:text-blue-700 font-serif">Contact Us</a></li>
+            <li><a href="/blog" className="">Blog</a></li>
+            <li><a href="/about" >About Us</a></li>
+            <li><a href="/contact" >Contact Us</a></li>
+            <li><a href="/contact" >About Ad</a></li>
+            <li><a href="/contact" >Accessibility</a></li>
+            <li><a href="/contact" >Safety & Privacy Center
+</a></li>
           </ul>
         </div>
         {/* Address */}
         <div className="box bg-transparent p-4">
-          <h6 className="text-lg">Address</h6>
-          <p className="text-sm">Sesamstreet 4</p>
-          <p className="text-sm">New York, 21345</p>
+          <h6 className="text-lg font-semibold">Address</h6>
+          <p className="text-sm">Sesam str. 4</p>
+          <p className="text-sm">Berlin, 21345</p>
         </div>
         {/* Contact */}
-        <div className="box bg-transparent p-4">
+        <div className="box bg-transparent p-4 font-semibold">
           <h6 className="text-lg ">Contact</h6>
           <p className="text-sm">Phone: 012345678</p>
-          <p className="text-sm">Email: a@acom.com</p>
+          <p className="text-sm">Email: contact@deetify.com</p>
         </div>
         {/* Follow Us */}
         <div className="box bg-transparent p-4">
-          <h6 className="text-lg mb-2">Follow Us</h6>
+          <h6 className="text-lg mb-2 font-semibold">Follow Us</h6>
           <div className="flex space-x-4">
             <img src="https://img.icons8.com/?size=64&id=119026&format=png" alt="Instagram" className="w-6 h-6" />
             <img src="https://img.icons8.com/?size=48&id=13912&format=png" alt="Facebook" className="w-6 h-6" />
             <img src="https://img.icons8.com/?size=50&id=439&format=png" alt="Google+" className="w-6 h-6" />
           </div>
         </div>
+        
       </div>
       {/* Second row */}
-      <div className="box bg-transparent text-center py-4 mt-4 font-serif">
-        <p className="text-sm">© CrossOver#1, Adehogun, Patrick, Adri</p>
+      <hr />
+      <div className="box bg-transparent text-center py-4 mt-4 ">
+        <p className="text-sm ">© CrossOver Group 3, Adehogun, Patrick,  Adri</p>
       </div>
     </footer>
   );
--- psd-project/src/components/Header.jsx
+++ psd-project/src/components/Header.jsx
@@ -7,16 +7,16 @@ const Navbar = () => {
           <span className="font-bold text-xl ml-9">DEETIFY</span>
         </div>
         <ul className="flex space-x-8 font-semi-bold text-lg mr-10 gap-4">
-          <li>
+          <li className="hover:underline">
             <a href="#">HOME</a>
           </li>
-          <li>
+          <li className="hover:underline">
             <a href="#">LIBRARY</a>
           </li>
-          <li>
+          <li className="hover:underline">
             <a href="#">UPGRADE</a>
           </li>
-          <li>
+          <li className="hover:underline">
             <button className='text-white bg-black rounded-md pb-2 px-8  hover:bg-white hover:text-black transition duration-300 ease-in-out text-center'>
               Login
             </button>
